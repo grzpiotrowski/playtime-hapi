5ccba4f3ea8d863a0bf2853d6ded08d1869113a7
Grzegorz Piotrowski <geopiotrowski@gmail.com>
2023-01-31 21:45:37 +0000

    Added more user tests

--- test/user-model-test.js
+++ test/user-model-test.js
@@ -45,4 +45,30 @@ suite("User API tests", () => {
     const deletedUser = await db.userStore.getUserById(testUsers[0]._id);
     assert.isNull(deletedUser);
   });
+
+  test("get a user - failures", async () => {
+    const noUserWithId = await db.userStore.getUserById("123");
+    assert.isNull(noUserWithId);
+    const noUserWithEmail = await db.userStore.getUserByEmail("no@one.com");
+    assert.isNull(noUserWithEmail);
+  });
+
+  test("get a user - bad params", async () => {
+    let nullUser = await db.userStore.getUserByEmail("");
+    assert.isNull(nullUser);
+    nullUser = await db.userStore.getUserById("");
+    assert.isNull(nullUser);
+    nullUser = await db.userStore.getUserById();
+    assert.isNull(nullUser);
+  });
+
+  test("delete One User - fail", async () => {
+    for (let i = 0; i < testUsers.length; i += 1) {
+      // eslint-disable-next-line no-await-in-loop
+      await db.userStore.addUser(testUsers[i]);
+    }
+    await db.userStore.deleteUserById("bad-id");
+    const allUsers = await db.userStore.getAllUsers();
+    assert.equal(testUsers.length, allUsers.length);
+  });
 });
