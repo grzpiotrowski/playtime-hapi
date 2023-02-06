06c79f8463453c84e3e500a7d843bd80940c60ce
Grzegorz Piotrowski <geopiotrowski@gmail.com>
2023-02-06 13:24:36 +0000

    Added switching stores in db facade

--- src/models/db.js
+++ src/models/db.js
@@ -1,3 +1,6 @@
+import { userMemStore } from "./mem/user-mem-store.js";
+import { playlistMemStore } from "./mem/playlist-mem-store.js";
+import { trackMemStore } from "./mem/track-mem-store.js";
 import { userJsonStore } from "./json/user-json-store.js";
 import { playlistJsonStore } from "./json/playlist-json-store.js";
 import { trackJsonStore } from "./json/track-json-store.js";
@@ -7,9 +10,17 @@ export const db = {
   playlistStore: null,
   trackStore: null,
 
-  init() {
-    this.userStore = userJsonStore;
-    this.playlistStore = playlistJsonStore;
-    this.trackStore = trackJsonStore;
+  init(storeType) {
+    switch (storeType) {
+      case "json":
+        this.userStore = userJsonStore;
+        this.playlistStore = playlistJsonStore;
+        this.trackStore = trackJsonStore;
+        break;
+      default:
+        this.userStore = userMemStore;
+        this.playlistStore = playlistMemStore;
+        this.trackStore = trackMemStore;
+    }
   },
 };
