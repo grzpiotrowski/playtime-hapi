cbac259a1094475fc9133091d1c9fe1fa6ce86a0
Grzegorz Piotrowski <geopiotrowski@gmail.com>
2023-02-18 12:17:48 +0000

    Extended playlist API

--- src/api/playlist-api.js
+++ src/api/playlist-api.js
@@ -1,29 +1,65 @@
 /* eslint-disable no-empty-function */
 import Boom from "@hapi/boom";
+import { PlaylistSpec } from "../models/joi-schemas.js";
 import { db } from "../models/db.js";
 
 export const playlistApi = {
   find: {
     auth: false,
     handler: async function (request, h) {
+      try {
+        const playlists = await db.playlistStore.getAllPlaylists();
+        return playlists;
+      } catch (err) {
+        return Boom.serverUnavailable("Database Error");
+      }
     },
   },
 
   findOne: {
     auth: false,
     async handler(request) {
+      try {
+        const playlist = await db.playlistStore.getPlaylistById(request.params.id);
+        if (!playlist) {
+          return Boom.notFound("No Playlist with this id");
+        }
+        return playlist;
+      } catch (err) {
+        return Boom.serverUnavailable("No Playlist with this id");
+      }
     },
   },
 
   create: {
     auth: false,
     handler: async function (request, h) {
+      try {
+        const playlist = request.payload;
+        const newPlaylist = await db.playlistStore.addPlaylist(playlist);
+        if (newPlaylist) {
+          return h.response(newPlaylist).code(201);
+        }
+        return Boom.badImplementation("error creating playlist");
+      } catch (err) {
+        return Boom.serverUnavailable("Database Error");
+      }
     },
   },
 
   deleteOne: {
     auth: false,
     handler: async function (request, h) {
+      try {
+        const playlist = await db.playlistStore.getPlaylistById(request.params.id);
+        if (!playlist) {
+          return Boom.notFound("No Playlist with this id");
+        }
+        await db.playlistStore.deletePlaylistById(playlist._id);
+        return h.response().code(204);
+      } catch (err) {
+        return Boom.serverUnavailable("No Playlist with this id");
+      }
     },
   },
 
@@ -38,5 +74,4 @@ export const playlistApi = {
       }
     },
   },
-
 };
--- test/api/playlist-api-test.js
+++ test/api/playlist-api-test.js
@@ -1,10 +1,8 @@
-import { EventEmitter } from "events";
 import { assert } from "chai";
 import { playtimeService } from "./playtime-service.js";
 import { assertSubset } from "../test-utils.js";
-import { maggie, mozart, testPlaylists } from "../fixtures.js";
 
-EventEmitter.setMaxListeners(25);
+import { maggie, mozart, testPlaylists } from "../fixtures.js";
 
 suite("Playlist API tests", () => {
 
@@ -20,14 +18,42 @@ suite("Playlist API tests", () => {
   teardown(async () => {});
 
   test("create playlist", async () => {
+    const returnedPlaylist = await playtimeService.createPlaylist(mozart);
+    assert.isNotNull(returnedPlaylist);
+    assertSubset(mozart, returnedPlaylist);
   });
 
   test("delete a playlist", async () => {
+    const playlist = await playtimeService.createPlaylist(mozart);
+    const response = await playtimeService.deletePlaylist(playlist._id);
+    assert.equal(response.status, 204);
+    try {
+      const returnedPlaylist = await playtimeService.getPlaylist(playlist.id);
+      assert.fail("Should not return a response");
+    } catch (error) {
+      assert(error.response.data.message === "No Playlist with this id", "Incorrect Response Message");
+    }
   });
 
   test("create multiple playlists", async () => {
+    for (let i = 0; i < testPlaylists.length; i += 1) {
+      testPlaylists[i].userid = user._id;
+      // eslint-disable-next-line no-await-in-loop
+      await playtimeService.createPlaylist(testPlaylists[i]);
+    }
+    let returnedLists = await playtimeService.getAllPlaylists();
+    assert.equal(returnedLists.length, testPlaylists.length);
+    await playtimeService.deleteAllPlaylists();
+    returnedLists = await playtimeService.getAllPlaylists();
+    assert.equal(returnedLists.length, 0);
   });
 
   test("remove non-existant playlist", async () => {
+    try {
+      const response = await playtimeService.deletePlaylist("not an id");
+      assert.fail("Should not return a response");
+    } catch (error) {
+      assert(error.response.data.message === "No Playlist with this id", "Incorrect Response Message");
+    }
   });
 });
